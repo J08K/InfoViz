2e92dd84ce6b2ef77df574b72b055926315fa2ce
Job van der Wal <j08k0l@gmail.com>
2024-06-28 15:04:58 +0200

    Voor het eerst kies ik voor light mode

---
+++ static/enforce-light-theme.js
@@ -0,0 +1,22 @@
+// Code from https://github.com/p-adema/info-vis/blob/main/static/js/enforce-light-theme.js
+
+function enforceLightMode() {
+  // Remove theme switcher, disallowing dark mode.
+  const switchButton = document.querySelector('.theme-switch-button');
+  if (typeof switchButton !== 'undefined') {
+    switchButton.remove();
+  }
+
+  // Enforce light mode, because that is when our story looks the best :)
+  if (document.querySelector('html').getAttribute('data-theme') !== 'light') {
+    // Switch theme right now.
+    document.querySelector('html').setAttribute('data-theme', 'light')
+
+    // Set light mode to be permanent for future visits.
+    localStorage.setItem('mode', 'light');
+    localStorage.setItem('theme', 'light');
+
+  }
+}
+
+window.addEventListener('load', enforceLightMode);
